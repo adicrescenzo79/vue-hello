4dde750a292a0869037a559eca658533af052a9d
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-03-23 15:28:36 +0100

    file iniziali

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// SCOMMENTARE SE SI USA JQUERY
+// $(document).ready(function(){
+//
+// });
+
+// SCOMMENTARE SE SI USA VUE
+var app = new Vue({
+  el: '#root',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
